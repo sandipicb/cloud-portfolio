7c3bf401c87f80d78d0badb4615a6cfb7863ce79
Sandip <sandipicb@gmail.com>
2026-09-07 16:09:56 +0600

    test hardened CI/CD deployment

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ function Projects() {
 
     return (
         <section className="projects" id="projects">
-            <h2>Projects</h2>
+            <h2>Projects & Cloud Engineering</h2>
 
             <div className="project-grid">
                 {projects.map((project, index) => (
